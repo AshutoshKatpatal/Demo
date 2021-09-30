38facaa90cdc6ea8ce41b999a51b8f30e559527f
AshutoshKatpatal <55042746+AshutoshKatpatal@users.noreply.github.com>
2021-09-30 14:54:48 +0530

    Add files via upload

---
+++ Demo/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo_proj.Models;
+
+namespace Demo_proj.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public List<UserModel> PutValue()
+        {
+            var users = new List<UserModel>
+            {
+                new UserModel{id=1,username="ashutosh",password="abcd1234"},
+                new UserModel{id=2,username="sonu",password="qwert12"}
+            };
+
+            return users;
+        }
+
+        [HttpPost]
+        public IActionResult Verify(UserModel usr)
+        {
+            var u = PutValue();
+
+            var ue = u.Where(u => u.username.Equals(usr.username));
+
+            var up = ue.Where(p => p.password.Equals(usr.password));
+
+            if (up.Count() == 1)
+            {
+                ViewBag.message = "Login Success !!";
+                return View("LoginSuccess");
+            }
+            else
+            {
+                ViewBag.message = "Login Faied !!";
+                return View("Login");
+            }
+        }
+
+    }
+}
